40f716f60e5db124d671d20e5fad1caf9c1e10ef
Justin Deffenbaugh <judeffen@judeffen-thinkpadx1nanogen2.rht.csb>
2026-02-04 22:14:46 -0500

    big map done

--- maps/map.py
+++ maps/map.py
@@ -1,23 +1,8 @@
-"""flask_example.py
-
-Required packages:
-- flask
-- folium
-
-Usage:
-
-Start the flask server by running:
-
-    $ python flask_example.py
-
-And then head to http://127.0.0.1:5000/ in your browser to see the map displayed
-
-"""
-
 from flask import Flask, render_template_string
 
 import folium
 import requests
+import json
 
 app = Flask(__name__)
 
@@ -31,22 +16,44 @@ def fullscreen():
 
 @app.route("/iframe")
 def iframe():
+    nc_center = [35.7596, -79.0193]
+    nc_bounds = [[33.7, -84.4], [36.6, -75.3]]
+    
+    m = folium.Map(
+        location=nc_center, 
+        max_bounds=True,
+        min_lat=nc_bounds[0][0], 
+        max_lat=nc_bounds[1][0],
+        min_lon=nc_bounds[0][1], 
+        max_lon=nc_bounds[1][1],
+        zoom_start=6.8,
+        zoom_snap=0.1
+    )
 
-    geo_json_data = requests.get(
-    "https://raw.githubusercontent.com/glynnbird/usstatesgeojson/refs/heads/master/north%20carolina.geojson"
-    ).json()
-
-    m = folium.Map(location=(35.53126396839661, -79.52843330891112), zoom_start=6)
-    folium.GeoJson(geo_json_data).add_to(m)
-
-    with open('data/po.json', 'r') as f:
+    with open('po_latlong.json', 'r') as f:
         data = json.load(f)
         for item in data:
+            marker_color = 'blue' if item['visited'] == "true" else 'cadetblue'
+            google_link = f"https://www.google.com/maps/search/?api=1&query={item['latitude']},{item['longitude']}"
+
+            popup_text = f"""
+            <h4>{item['city']}</h4>
+            <b>Street:</b> {item['street']}<br>
+            <b>Zip:</b> {item['zip']}<br>
+            <b>Status:</b> {'✅ SEENT' if item['visited'] == 'true' else '❌ NOT SEENT'}<br>
+            <a href="{google_link}" target="_blank" style="color: blue; text-decoration: underline;">
+                Get Directions
+            </a>
+            """
+            
+            pop_iframe = folium.IFrame(popup_text, width=200, height=150)
+            popup = folium.Popup(pop_iframe, max_width=200)
  
             folium.Marker(
-                location=[item[latitude], item[longitude]],
-                tooltip=item[city],
-                popup=item[city],
+                location=[float(item['latitude']), float(item['longitude'])],
+                tooltip=item['city'].title(),
+                popup=popup,
+                icon=folium.Icon(color=marker_color, icon='envelope'),   
             ).add_to(m)
 
     # set the iframe width and height
@@ -69,40 +76,67 @@ def iframe():
     )
 
 
-@app.route("/components")
-def components():
-    """Extract map components and put those on a page."""
+@app.route("/<zip>")
+def zip():
+
+    nc_center = [35.7596, -79.0193]
+    nc_bounds = [[33.7, -84.4], [36.6, -75.3]]
+    
     m = folium.Map(
-        width=800,
-        height=600,
+        location=nc_center, 
+        max_bounds=True,
+        min_lat=nc_bounds[0][0], 
+        max_lat=nc_bounds[1][0],
+        min_lon=nc_bounds[0][1], 
+        max_lon=nc_bounds[1][1],
+        zoom_start=6.8,
+        zoom_snap=0.1
     )
 
-    m.get_root().render()
-    header = m.get_root().header.render()
-    body_html = m.get_root().html.render()
-    script = m.get_root().script.render()
+    with open('po_latlong.json', 'r') as f:
+        data = json.load(f)
+        for item in data:
+            marker_color = 'blue' if item['visited'] == "true" else 'cadetblue'
+            google_link = f"https://www.google.com/maps/search/?api=1&query={item['latitude']},{item['longitude']}"
+
+            popup_text = f"""
+            <h4>{item['city']}</h4>
+            <b>Street:</b> {item['street']}<br>
+            <b>Zip:</b> {item['zip']}<br>
+            <b>Status:</b> {'✅ SEENT' if item['visited'] == 'true' else '❌ NOT SEENT'}<br>
+            <a href="{google_link}" target="_blank" style="color: blue; text-decoration: underline;">
+                Get Directions
+            </a>
+            """
+            
+            pop_iframe = folium.IFrame(popup_text, width=200, height=150)
+            popup = folium.Popup(pop_iframe, max_width=200)
+ 
+            folium.Marker(
+                location=[float(item['latitude']), float(item['longitude'])],
+                tooltip=item['city'].title(),
+                popup=popup,
+                icon=folium.Icon(color=marker_color, icon='envelope'),   
+            ).add_to(m)
+
+    # set the iframe width and height
+    m.get_root().width = "800px"
+    m.get_root().height = "600px"
+    iframe = m.get_root()._repr_html_()
 
     return render_template_string(
         """
             <!DOCTYPE html>
             <html>
-                <head>
-                    {{ header|safe }}
-                </head>
+                <head></head>
                 <body>
-                    <h1>Using components</h1>
-                    {{ body_html|safe }}
-                    <script>
-                        {{ script|safe }}
-                    </script>
+                    <h1>Using an iframe</h1>
+                    {{ iframe|safe }}
                 </body>
             </html>
         """,
-        header=header,
-        body_html=body_html,
-        script=script,
+        iframe=iframe,
     )
-
 
 if __name__ == "__main__":
     app.run(debug=True)
