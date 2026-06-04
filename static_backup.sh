07f41962156e22ea097e7cd1b40cade0426e6390
Justin Deffenbaugh <judeffen@judeffen-thinkpadx1nanogen2.rht.csb>
2026-06-04 13:48:40 -0400

    Change backup script

--- static_backup.sh
+++ static_backup.sh
@@ -8,7 +8,7 @@ DATE=`date "+%Y-%m-%d %H:%M:%S"`
 
 # Only run if the flag file exists
 if [ -f "$FLAG_FILE" ]; then
-    if tar -czf "$TAR_FILE" -C "$APP_DIR"/data po_backup.json /var/www/html/static/; then
+    if tar -czf "$TAR_FILE" -C "$APP_DIR"/data po_backup.json /var/www/html/static/.; then
         printf "%s Backup successful\n" "$DATE" >> "$APP_DIR"/logs/backup.log
         rm "$FLAG_FILE"
     else
