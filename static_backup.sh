b4275228dad02e8dec1d4e8c66d19f3fdeab0434
Justin Deffenbaugh <judeffen@judeffen-thinkpadx1nanogen2.rht.csb>
2026-06-04 12:25:42 -0400

    Get the path right

--- static_backup.sh
+++ static_backup.sh
@@ -7,6 +7,6 @@ TAR_FILE="backup.tar.gz"
 
 # Only run if the flag file exists
 if [ -f "$FLAG_FILE" ]; then
-    tar -czf "$TAR_FILE" -C "$APP_DIR"/data po_backup.json /var/www/html/static/images/
+    tar -czf "$TAR_FILE" -C "$APP_DIR"/data po_backup.json /var/www/html/static/
     rm "$FLAG_FILE"
 fi
